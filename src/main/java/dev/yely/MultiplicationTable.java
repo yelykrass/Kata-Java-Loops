7ec2b9d844b9567784c3e499c91417705bcc1841
Yelyzaveta <lizakrasyuk24@gmail.com>
2025-08-11 16:36:43 +0200

    Add method generateTable

--- src/main/java/dev/yely/MultiplicationTable.java
+++ src/main/java/dev/yely/MultiplicationTable.java
@@ -1,5 +1,16 @@
 package dev.yely;
 
-public class MultiplicationTable {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MultiplicationTable {
+    public List<String> generateTable(int numb) {
+        List<String> table = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            int multiplicationResult = numb * i;
+            String row = "%d x %d = %d".formatted(numb, i, multiplicationResult);
+            table.add(row);
+        }
+        return table;
+    }
 }
